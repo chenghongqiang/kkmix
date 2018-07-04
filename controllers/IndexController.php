2ec56bf5200cf7ced4065aa00f38bc642a420815
chenghongqiang <kewin.cheng@jfz.com>
2018-07-04 18:10:21 +0800

    file add

---
+++ controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kewin.cheng
+ * Date: 2018/7/4
+ * Time: 17:52
+ */
+
+class IndexController
+{
+    public function actionIndex()
+    {
+        echo 2;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The Index Of Framework
+ * User: kewin.cheng
+ * Date: 2018/7/4
+ *
+ */
+
+defined('__ROOTDIR__') or define('__ROOTDIR__', __DIR__ . '/../');
+
+require_once __ROOTDIR__ . 'vendor/autoload.php';
+
+var_dump($_GET);
+
+
